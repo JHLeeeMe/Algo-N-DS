c4e4a80a47d98a210c3155c1fd325cb576e87a7d
JHLeeeMe-T420 <lejung92@gmail.com>
2020-06-29 22:02:32 +0900

    Update all methods
    	수정함:        dataStructure/MyQueue.java

--- dataStructure/MyQueue.java
+++ dataStructure/MyQueue.java
@@ -11,44 +11,115 @@ interface InnerMyQueue<T> {
 public class MyQueue<T> implements InnerMyQueue<T> {
     private Node<T> front;
     private Node<T> rear;
+    private int size = 0;
 
     private class Node<T> {
         private T data;
         private Node<T> next;
 
-        Node(T item) {
+        public Node(T item) {
             this.data = item;
         }
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.front == null;
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-
+        front = rear= null;
+        size = 0;
     }
 
     @Override
     public void enqueue(T item) {
-        // TODO Auto-generated method stub
-
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty())
+            front = newNode;
+        else
+            rear.next = newNode;
+        rear = newNode;
+        size++;
     }
 
     @Override
     public T dequeue() {
-        // TODO Auto-generated method stub
-        return null;
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            throw new java.util.NoSuchElementException();
+        } else {
+            T tmp = front.data;
+            front = front.next;
+            size--;
+            return tmp;
+        }
     }
 
     @Override
     public T peek() {
-        // TODO Auto-generated method stub
-        return null;
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            throw new java.util.NoSuchElementException();
+        } else {
+            return front.data;
+        }
     }
-    
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void printAll() {
+        if (front == null) {
+            System.out.println("Queue is Empty");
+        } else {
+            Node<T> tmp = front;
+            System.out.print("[");
+            for (int i=0; i<size; i++) {
+                System.out.print(tmp.data + ", ");
+                tmp = tmp.next;
+            }
+            System.out.println("]");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<>();
+        System.out.println(queue.getSize());
+        System.out.println(queue.isEmpty());
+        queue.printAll();
+        System.out.println("==============================");
+
+        // System.out.println(queue.peek());
+        // System.out.println(queue.dequeue());
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.printAll();
+        System.out.println(queue.getSize());
+        System.out.println("==============================");
+
+        System.out.println(queue.peek());
+        System.out.println(queue.getSize());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.getSize());
+        System.out.println("==============================");
+
+        System.out.println(queue.front.data);
+        System.out.println(queue.rear.data);
+        queue.enqueue(90);
+        queue.printAll();
+        System.out.println(queue.isEmpty());
+        System.out.println("==============================");
+
+        queue.clear();
+        queue.printAll();
+        System.out.println(queue.getSize());
+        System.out.println(queue.isEmpty());
+    }
 }
