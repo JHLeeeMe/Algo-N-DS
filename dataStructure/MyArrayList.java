b35bfb5e1acad21742de465b5d07d74729644826
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-12 13:51:44 +0900

    Update all methods
    	수정함:        dataStructure/MyArrayList.java

--- dataStructure/MyArrayList.java
+++ dataStructure/MyArrayList.java
@@ -1,10 +1,12 @@
 package dataStructure;
 
+import java.util.ArrayList;
+
 interface InnerMyArrayList<T> {
     boolean isEmpty();
     void clear();
     void add(T item);
-    T remove();
+    T remove(int idx);
 }
 
 public class MyArrayList<T> implements InnerMyArrayList<T> {
@@ -38,15 +40,78 @@ public class MyArrayList<T> implements InnerMyArrayList<T> {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void add(T item) {
-        if (getSize() == this.capacity)
-            capacity = capacity * 2;
+        if (getSize() == this.capacity) {
+            T[] newArr = (T[]) new Object[capacity << 1];
+            for (int i=0; i<elementCount; i++) {
+                newArr[i] = arr[i];
+            }
+            this.arr = newArr;
+        }
         arr[elementCount++] = item;
     }
 
+    @SuppressWarnings("unchecked")
+    public void add(int idx, T item) {
+        if (idx < 0 || idx > this.capacity-1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+
+        if (getSize() == this.capacity) {
+            T[] newArr = (T[]) new Object[capacity << 1];
+            for (int i=0; i<elementCount; i++) {
+                newArr[i] = arr[i];
+            }
+            this.arr = newArr;
+        }
+
+        for (int i=elementCount; i>=idx; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[idx] = item;
+        elementCount++;
+    }
+
     @Override
-    public T remove() {
-        return null;
+    public T remove(int idx) {
+        if (idx < 0 || idx >= getSize()) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+
+        T tmp = arr[idx];
+        arr[idx] = null;
+        for (int i=idx; i<getSize(); i++) {
+            arr[i] = arr[i+1];
+        }
+        elementCount--;
+        return tmp;
+    }
+    public T removeByItem(Object o) {
+        if (o == null) {
+            throw new java.lang.IllegalStateException();
+        } else {
+            int idx = indexOf(o);
+            if (idx == -1) {
+                throw new java.util.NoSuchElementException();
+            }
+            return remove(idx);
+        }
+    }
+
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int idx=0; idx<elementCount; idx++) {
+                if (this.arr[idx] == null)
+                    return idx;
+            }
+        } else {
+            for (int idx=0; idx<elementCount; idx++ ) {
+                if (this.arr[idx].equals(o))
+                    return idx;
+            }
+        }
+        return -1;
     }
 
     public int getSize() {
