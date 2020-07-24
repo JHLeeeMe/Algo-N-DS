64e8033d0196a15148c8c06e48e5a8b85825eaeb
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-24 18:56:44 +0900

    [Update] unittest & typo
    	수정함:        python/data_structure/my_linked_list.py

--- python/data_structure/my_linked_list.py
+++ python/data_structure/my_linked_list.py
@@ -5,16 +5,15 @@ from abc import ABC, abstractmethod
 class InnerLinkedList(ABC):
     @abstractmethod
     def is_empty(self) -> bool: ...
+
     @abstractmethod
     def add(self, item): ...
+
     @abstractmethod
     def remove(self): ...
+
     @abstractmethod
-    def remove_item(self, item): ...
-    @abstractmethod
-    def __index_of(self, item) -> int: ...
-    @abstractmethod
-    def print_list(self): ...
+    def print_all(self): ...
 
 
 class MyLinkedList(InnerLinkedList):
@@ -24,8 +23,8 @@ class MyLinkedList(InnerLinkedList):
 
     class Node:
         def __init__(self, item):
-            self.__data = item
-            self.__next = None
+            self.data = item
+            self.next = None
 
     def is_empty(self):
         return self.__size == 0
@@ -39,9 +38,9 @@ class MyLinkedList(InnerLinkedList):
             self.__head = new_node
         else:
             curr = self.__head
-            while curr.__next is not None:
-                curr = curr.__next
-            curr.__next = new_node
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new_node
         self.__size += 1
 
     def add_into_index(self, idx, item):
@@ -51,12 +50,12 @@ class MyLinkedList(InnerLinkedList):
 
         new_node = self.Node(item)
         if idx == 0:
-            new_node.__next = self.__head
+            new_node.next = self.__head
             self.__head = new_node
         else:
             before_node = self.__node(idx - 1)
-            new_node.__next = before_node.__next
-            before_node.__next = new_node
+            new_node.next = before_node.next
+            before_node.next = new_node
         self.__size += 1
 
     def remove(self):
@@ -64,10 +63,11 @@ class MyLinkedList(InnerLinkedList):
 
     def __remove_first(self):
         if self.__size == 0:
+            print('List is Empty.')
             return None
 
-        tmp = self.__head.__data
-        self.__head = self.__head.__next
+        tmp = self.__head.data
+        self.__head = self.__head.next
         self.__size -= 1
         return tmp
 
@@ -80,13 +80,13 @@ class MyLinkedList(InnerLinkedList):
             return self.__remove_first()
 
         before_node = self.__node(idx - 1)
-        tmp = before_node.__next.__data
-        before_node.__next = before_node.__next.__next
+        tmp = before_node.next.data
+        before_node.next = before_node.next.next
         self.__size -= 1
         return tmp
 
     def remove_by_item(self, item):
-        idx = self.__idex_of(item)
+        idx = self.__index_of(item)
         if idx != -1:
             return self.remove_by_index(idx)
         return None
@@ -95,30 +95,109 @@ class MyLinkedList(InnerLinkedList):
         curr = self.__head
         idx = 0
         while curr is not None:
-            if curr.__data == item:
+            if curr.data == item:
                 return idx
-            curr = curr.__next
+            curr = curr.next
             idx += 1
-        print('data ' + str(item) + 'is not in linkedlist')
+        print('data ' + str(item) + ' is not in linkedlist')
         return -1
 
     def __node(self, idx):
+        if not isinstance(idx, int):
+            raise TypeError('idx arg must be int type')
+
         curr = self.__head
         for i in range(idx):
-            curr = curr.__next
+            curr = curr.next
         return curr
 
     def size(self):
         return self.__size
 
-    def print_list(self):
+    def print_all(self):
         tmp = []
         curr = self.__head
         while curr is not None:
-            tmp.append(curr.__data)
-            curr = curr.__next
+            tmp.append(curr.data)
+            curr = curr.next
         print(tmp)
+
+    def __iter__(self):
+        """Returns the Iterator object."""
+        return self.MyIterator(self.__head)
+
+    class MyIterator:
+        """Iterator class"""
+        def __init__(self, head):
+            self.__curr = head
+
+        def __next__(self):
+            """Returns the next value from list"""
+            if self.__curr is None:
+                raise StopIteration
+
+            tmp = self.__curr.data
+            self.__curr = self.__curr.next
+            return tmp
 
 
 class MyLinkedListTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.my_list = MyLinkedList()
+
+    def test_empty(self):
+        print()
+        print('################')
+        print('# test_empty() #')
+        print('################')
+
+        self.assertEqual(self.my_list.remove(), None)
+        self.assertEqual(self.my_list.remove_by_item('aaaa'), None)
+
+        with self.assertRaises(IndexError):
+            self.my_list.remove_by_index(1)
+
+        with self.assertRaises(TypeError):
+            self.my_list._MyLinkedList__node('aaa')
+
+    def test_all(self):
+        print()
+        print('##############')
+        print('# test_all() #')
+        print('##############')
+
+        self.my_list.add(1)
+        self.my_list.add(2)
+        self.my_list.add_into_index(0, 3)
+        self.my_list.print_all()
+
+        with self.assertRaises(IndexError):
+            self.my_list.add_into_index(-1, 123)
+
+        with self.assertRaises(IndexError):
+            self.my_list.add_into_index(1123123, 123)
+
+        self.my_list.add(12345)
+        self.assertEqual(self.my_list.size(), 4)
+        self.my_list.print_all()
+        self.assertEqual(self.my_list.remove_by_item(12345), 12345)
+        self.my_list.print_all()
+
+        with self.assertRaises(IndexError):
+            self.my_list.remove_by_index(3)
+
+    def test_iterator(self):
+        print()
+        print('###################')
+        print('# test_iterator() #')
+        print('###################')
+
+        self.my_list.add(1)
+        self.my_list.add(2)
+        self.my_list.add(3)
+        self.my_list.add(4)
+        self.my_list.add(5)
+        self.my_list.print_all()                      # prints [1, 2, 3, 4, 5]
+
+        for item in self.my_list:
+            print(item)
