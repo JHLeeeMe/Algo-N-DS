6333029ab58595e0e81a4c6385fe4692126723e6
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-25 12:23:04 +0900

    [Update] all methods
    	수정함:        python/algorithm/search/binary_search.py

--- python/algorithm/search/binary_search.py
+++ python/algorithm/search/binary_search.py
@@ -2,8 +2,39 @@ import unittest
 
 
 class BinarySearch:
-    pass
+    def search(self, arr, item: int) -> int:
+        low = 0
+        high = len(arr) - 1
+        mid = None
+
+        while low <= high:
+            mid = (low + high) >> 1
+            if item == arr[mid]:
+                return mid
+            elif item < arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
+
+    def __rec_search(self, arr, item: int,
+                     low: int, high: int) -> int:
+        if (low > high):
+            return -1
+
+        mid = (low + high) >> 1
+        if item == arr[mid]:
+            return mid
+        elif item < arr[mid]:
+            self.rec_search(arr, item, low, mid - 1)
+        else:
+            self.rec_search(arr, item, mid + 1, high)
 
 
 class BinarySearchTest(unittest.TestCase):
-    pass
+    def test_search(self):
+        pass
+
+    def test_rec_search(self):
+        pass
