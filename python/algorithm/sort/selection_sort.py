521a6d011a18c811896525b966e0947ae62ffa5a
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 18:23:29 +0900

    [Add] selection_sort.py blueprint
    	새 파일:       python/algorithm/sort/selection_sort.py

---
+++ python/algorithm/sort/selection_sort.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def selection_sort(lst: list) -> None:
+    pass
+
+
+class SelectionSortTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_selection_sort(self):
+        pass
