d616092f39a43b837c9c5ca4a2817021688cdb35
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 16:52:06 +0900

    [Update] insertion_sort() & test case
    	수정함:        python/algorithm/sort/insertion_sort.py

--- python/algorithm/sort/insertion_sort.py
+++ python/algorithm/sort/insertion_sort.py
@@ -1,10 +1,22 @@
 import unittest
 
 
-def insertion_sort(lst: list) -> None:
-    pass
+def insertion_sort(lst: list) -> list:
+    for i in range(1, len(lst)):
+        tmp = lst[i]
+        j = i - 1
+
+        while j >= 0 and tmp < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+
+        lst[j + 1] = tmp
 
 
 class InsertionSortTest(unittest.TestCase):
+    def setUp(self):
+        self.lst = [4, 2, 1, 30, 24, 32, 5, 7]
+
     def insertion_sort_test(self):
-        pass
+        insertion_sort(self.lst)
+        self.assertListEqual(self.lst, [1, 2, 4, 5, 7, 24, 30, 32])
