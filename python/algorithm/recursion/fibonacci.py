988af6524fdb55101d59bb2a4e62c9f0f23861a8
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 15:57:24 +0900

    [Update] all methods & test case
    	수정함:        python/algorithm/recursion/fibonacci.py

--- python/algorithm/recursion/fibonacci.py
+++ python/algorithm/recursion/fibonacci.py
@@ -1,20 +1,36 @@
+"""Fibonacci
+
+0, 1, 1, 2, 3, 5, 8, ...
+"""
 import unittest
 
 
 def fibonacci(x: int) -> int:
-        pass
-        
+    if x <= 1:
+        return x
 
-def fibonacci_tailrec(x: int, result: int) -> int:
-    pass
+    return fibonacci(x - 2) + fibonacci(x - 1)
+
+
+def fibonacci_tailrec(x: int, a: int = 0, b: int = 1) -> int:
+    if x == 0:
+        return a
+    if x == 1:
+        return b
+
+    return fibonacci_tailrec(x - 1, b, a + b)
 
 
 class FibonacciTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.lst = []
 
     def test_fibonacci(self):
-        pass
+        for i in range(7):
+            self.lst.append(fibonacci(i))
+        self.assertListEqual(self.lst, [0, 1, 1, 2, 3, 5, 8])
 
     def test_fibonacci_tailrec(self):
-        pass
+        for i in range(7):
+            self.lst.append(fibonacci_tailrec(i))
+        self.assertListEqual(self.lst, [0, 1, 1, 2, 3, 5, 8])
