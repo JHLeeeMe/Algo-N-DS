3b88907e535e8f17cb873c7c1769cbc8fd50899a
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 19:20:03 +0900

    [Add] merge_sort.py blueprint
    	새 파일:       python/algorithm/sort/merge_sort.py

---
+++ python/algorithm/sort/merge_sort.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+def merge_sort(lst: list) -> None:
+    __sort(lst, 0, len(lst) - 1)
+
+
+def __sort(lst: list, a: int, b: int) -> None:
+    pass
+
+
+def merge(lst: list, left: int, mid: int, right: int) -> None:
+    pass
+
+
+class MergeSortTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_empty_list(self):
+        pass
+
+    def test_merge_sort(self):
+        pass
