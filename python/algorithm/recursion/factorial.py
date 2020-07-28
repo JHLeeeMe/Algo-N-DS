24f9620e1b8cf6369effa63356f88f450433eb5d
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 16:11:50 +0900

    [Update] all methods & test case
    	수정함:        python/algorithm/recursion/factorial.py

--- python/algorithm/recursion/factorial.py
+++ python/algorithm/recursion/factorial.py
@@ -1,24 +1,39 @@
 """Factorial
 
 0! = 1
-1! = 1
-2! = 2 * 1
-3! = 3 * 2 * 1
+1! = 1 * 0!
+2! = 2 * 1!
+3! = 3 * 2!
+...
 """
 import unittest
 
 
 def factorial(x: int) -> int:
-    pass
+    if x == 0:
+        return 1
+
+    return x * factorial(x - 1)
 
 
-def factorial_tailrec(x: int, result: int) -> int:
-    pass
+def factorial_tailrec(x: int, result: int = 1) -> int:
+    if x < 0:
+        return result
+
+    return factorial_tailrec(x - 1, x * result)
 
 
 class FactorialTest(unittest.TestCase):
     def factorial_test(self):
-        pass
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(2), 2)
+        self.assertEqual(factorial(3), 6)
+        self.assertEqual(factorial(4), 24)
 
     def factorial_tailrec_test(self):
-        pass
+        self.assertEqual(factorial_tailrec(0), 1)
+        self.assertEqual(factorial_tailrec(1), 1)
+        self.assertEqual(factorial_tailrec(2), 2)
+        self.assertEqual(factorial_tailrec(3), 6)
+        self.assertEqual(factorial_tailrec(4), 24)
