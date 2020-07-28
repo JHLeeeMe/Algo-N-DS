6262278dc5ff728825fa50e7542c4f1a9d0c22a2
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 16:02:29 +0900

    [Add] factorial.py blueprint
    	새 파일:       python/algorithm/recursion/factorial.py

---
+++ python/algorithm/recursion/factorial.py
@@ -0,0 +1,24 @@
+"""Factorial
+
+0! = 1
+1! = 1
+2! = 2 * 1
+3! = 3 * 2 * 1
+"""
+import unittest
+
+
+def factorial(x: int) -> int:
+    pass
+
+
+def factorial_tailrec(x: int, result: int) -> int:
+    pass
+
+
+class FactorialTest(unittest.TestCase):
+    def factorial_test(self):
+        pass
+
+    def factorial_tailrec_test(self):
+        pass
