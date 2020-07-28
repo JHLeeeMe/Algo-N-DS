bda8bcb49fbc3253e964431da46b0572496a2aa8
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-28 16:55:48 +0900

    [Add] bubble_sort.py blueprint
    	새 파일:       python/algorithm/sort/bubble_sort.py

---
+++ python/algorithm/sort/bubble_sort.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def bubble_sort(lst: list) -> None:
+    pass
+
+
+class BubbleSortTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_bubble_sort(self):
+        pass
