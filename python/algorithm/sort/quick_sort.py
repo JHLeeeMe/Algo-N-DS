72204b3df75061b2297bcff3f8cf2f892bc811f5
JHLeeeMe-T420 <lejung92@gmail.com>
2020-07-30 22:36:06 +0900

    [Add] quick_sort.py blueprint
    	새 파일:       python/algorithm/sort/quick_sort.py

---
+++ python/algorithm/sort/quick_sort.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def quick_sort(lst: list) -> None:
+    pass
+
+
+def __sort(lst: list, left: int, right: int) -> None:
+    pass
+
+
+def __partition(lst: list, left: int, right: int) -> int:
+    pass
+
+
+def __swap(lst: list, a: int, b: int):
+    pass
+
+
+class QuickSortTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_empty_list(self):
+        pass
+
+    def test_quick_sort(self):
+        pass
