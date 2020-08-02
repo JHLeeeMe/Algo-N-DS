246dbc24fd96305d638981ad47575098b9484bda
JHLeeeMe-T420 <lejung92@gmail.com>
2020-08-02 17:52:06 +0900

    [Update] all functions & test case
    	수정함:        python/algorithm/sort/heap_sort.py

--- python/algorithm/sort/heap_sort.py
+++ python/algorithm/sort/heap_sort.py
@@ -1,12 +1,14 @@
 import unittest
 
 
-def heap_sort(lst: list):
+def heap_sort(lst: list) -> None:
     n = len(lst)
 
     # create max heap
-    for i in reversed(range(n/2)):
-        heapify(lst, n, i)
+    # for i in reversed(range(n >> 1)):
+    #     heapify(lst, n, i)
+    for i in range((n >> 1)-1, -1, -1):
+        heapify(lst, heap_size=n, idx=i)
 
     # to asc_sorted_list
     for i in range(n - 1, 0, -1):
@@ -14,19 +16,36 @@ def heap_sort(lst: list):
         lst[0] = lst[i]
         lst[i] = tmp
 
-        heapify(lst, i, 0)
+        heapify(lst, heap_size=i, idx=0)
 
 
-def heapify(lst: list, heap_size: int, root_idx: int):
-    pass
+def heapify(lst: list, heap_size: int, idx: int) -> None:
+    root = idx
+    left = 2*idx + 1
+    right = 2*idx + 2
+
+    if left < heap_size and lst[left] > lst[root]:
+        root = left
+
+    if right < heap_size and lst[right] > lst[root]:
+        root = right
+
+    if root != idx:
+        tmp = lst[idx]
+        lst[idx] = lst[root]
+        lst[root] = tmp
+
+        heapify(lst, heap_size, root)
 
 
 class HeapSortTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.lst = [4, 10, 2, 1, 60, 23]
 
     def test_empty_list(self):
-        pass
+        empty_list = []
+        self.assertListEqual(empty_list, [])
 
     def test_all(self):
-        pass
+        heap_sort(self.lst)
+        self.assertListEqual(self.lst, [1, 2, 4, 10, 23, 60])
