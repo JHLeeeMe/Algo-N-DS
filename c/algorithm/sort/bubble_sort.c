215e1c1ba6f9a2cf0d7b523dccc3b1639ad3de6d
JHLeeeMe-T420 <lejung92@gmail.com>
2020-08-10 20:43:44 +0900

    [Add] c/
    	새 파일:       c/algorithm/sort/assembly/bubble_sort.s
    	새 파일:       c/algorithm/sort/bubble_sort.c
    	새 파일:       c/algorithm/sort/output/bubble_sort
    	새 파일:       c/algorithm/sort/pre_compile/bubble_sort.i

---
+++ c/algorithm/sort/bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size);
+
+int main(void)
+{
+    int arr[] = {5, 7, 1, 3, 2, 10, 9, 45};
+    int arr_length = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, arr_length);
+
+    for (int i = 0; i < arr_length; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void bubbleSort(int arr[], int size)
+{
+    for (int i = size - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
