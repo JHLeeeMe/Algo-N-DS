cc2263de73aa8a8c0c5d74d8c7c3bf6cdd0da3ea
JHLeeeMe-T420 <lejung92@gmail.com>
2020-08-11 15:07:04 +0900

    [Add] binary_search.c & .s
    	새 파일:       c/algorithm/search/assembly/binary_search.s
    	새 파일:       c/algorithm/search/binary_search.c

---
+++ c/algorithm/search/assembly/binary_search.s
@@ -0,0 +1,134 @@
+	.file	"binary_search.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"found!"
+.LC1:
+	.string	"Not found!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$64, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$1, -48(%rbp)
+	movl	$3, -44(%rbp)
+	movl	$5, -40(%rbp)
+	movl	$6, -36(%rbp)
+	movl	$10, -32(%rbp)
+	movl	$11, -28(%rbp)
+	movl	$14, -24(%rbp)
+	movl	$15, -20(%rbp)
+	movl	$50, -16(%rbp)
+	movl	$9, -52(%rbp)
+	movl	-52(%rbp), %eax
+	leal	-1(%rax), %edx
+	leaq	-48(%rbp), %rax
+	movl	$50, %ecx
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	binarySearch
+	testb	%al, %al
+	je	.L2
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L3:
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	binarySearch
+	.type	binarySearch, @function
+binarySearch:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	%edx, -32(%rbp)
+	movl	%ecx, -36(%rbp)
+	movl	-28(%rbp), %eax
+	cmpl	-32(%rbp), %eax
+	jle	.L7
+	movl	$0, %eax
+	jmp	.L8
+.L7:
+	movl	-32(%rbp), %edx
+	movl	-28(%rbp), %eax
+	addl	%edx, %eax
+	sarl	%eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -36(%rbp)
+	jne	.L9
+	movl	$1, %eax
+	jmp	.L8
+.L9:
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -36(%rbp)
+	jle	.L10
+	movl	-4(%rbp), %eax
+	leal	1(%rax), %esi
+	movl	-36(%rbp), %ecx
+	movl	-32(%rbp), %edx
+	movq	-24(%rbp), %rax
+	movq	%rax, %rdi
+	call	binarySearch
+	jmp	.L8
+.L10:
+	movl	-4(%rbp), %eax
+	leal	-1(%rax), %edi
+	movl	-36(%rbp), %edx
+	movl	-28(%rbp), %esi
+	movq	-24(%rbp), %rax
+	movl	%edx, %ecx
+	movl	%edi, %edx
+	movq	%rax, %rdi
+	call	binarySearch
+.L8:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	binarySearch, .-binarySearch
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ c/algorithm/search/binary_search.c
@@ -0,0 +1,37 @@
+/** Binary Search
+ *
+ * O(logN), Omega(1)
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool binarySearch(int nArr[], int nFirst, int nLast, int nTarget);
+
+int main(void)
+{
+    int nArr[] = {1, 3, 5, 6, 10, 11, 14, 15, 50};
+    int nArr_len = sizeof(nArr) / sizeof(nArr[0]);
+
+    if (binarySearch(nArr, 0, nArr_len - 1, 50))
+        printf("found!");
+    else
+        printf("Not found!");
+
+    return 0;
+}
+
+bool binarySearch(int nArr[], int nFirst, int nLast, int nTarget)
+{
+    if (nFirst > nLast)
+        return false;
+
+    int nMid = (nLast + nFirst) >> 1;
+
+    if (nArr[nMid] == nTarget)
+        return true;
+    else if (nArr[nMid] < nTarget)
+        return binarySearch(nArr, nMid + 1, nLast, nTarget);
+    else
+        return binarySearch(nArr, nFirst, nMid - 1, nTarget);
+}
