fc2833976c264cd96d1f8e0f3a574aa72be5c1b3
JHLeeeMe-T420 <lejung92@gmail.com>
2020-08-28 03:48:11 +0900

    [Add] unique_chars (bit vector test)
    	새 파일:       c/algorithm/unique_chars.c

---
+++ c/algorithm/unique_chars.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool
+isUniqueStr(char *c_ptr);
+
+int
+main(void)
+{
+    char *str1 = "Hello";
+    char *str2 = "abcde";
+
+    printf("Hello => ");
+    printf(isUniqueStr(str1) ? "유니크" : "중복");
+    printf("\n");
+    printf("abcde => ");
+    printf(isUniqueStr(str2) ? "유니크" : "중복");
+    printf("\n");
+}
+
+bool
+isUniqueStr(char *c_ptr)
+{
+    char *
+        str = c_ptr;
+
+    int bit_bucket = 0;
+
+    for (unsigned int i = 0; i < strlen(str); i++)
+    {
+        int n = str[i] - 'a';
+        if ((bit_bucket & (1 << n)) > 0)
+            return false;
+
+        bit_bucket |= (1 << n);
+    }
+    return true;
+}
