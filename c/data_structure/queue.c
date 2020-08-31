d47f406c9685da50cc4656203b559b379395cbc8
JHLeeeMe-T420 <lejung92@gmail.com>
2020-08-31 16:52:40 +0900

    [Update] is_empty() function
    	수정함:        c/data_structure/queue.c

--- c/data_structure/queue.c
+++ c/data_structure/queue.c
@@ -5,18 +5,36 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-bool is_empty();
-void clear();
-void enqueue(int32_t item);
-int32_t dequeue();
-int32_t peek();
-
 typedef struct Node
 {
     int32_t data;
     struct Node* next;
 } Node;
 
+bool is_empty(Node* front);
+void clear(Node* front);
+void enqueue(int32_t item);
+int32_t dequeue();
+int32_t peek();
+
 int32_t main(void)
 {
+    Node* front = NULL;
+    printf("empty Queue: %s\n", is_empty(front) ? "true" : "false");
+
+}
+
+bool is_empty(Node* front)
+{
+    return front == NULL;
+}
+
+void clear(Node* front)
+{
+    Node* tmp = NULL;
+    while (front->next != NULL) {
+        tmp = front->next;
+        front = tmp;
+    }
+    front = NULL;
 }
