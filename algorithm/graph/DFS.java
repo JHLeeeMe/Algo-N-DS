70f1637c7335775202f53f3ce399c9b603b4de35
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-19 16:39:27 +0900

    [Update] Graph 관련 코드 수정
    	수정함:        algorithm/graph/DFS.java

--- algorithm/graph/DFS.java
+++ algorithm/graph/DFS.java
@@ -2,44 +2,20 @@
  * DFS (Depth-First Search, 깊이 우선 탐색)
  *  1. stack을 활용한 구현
  *  2. 재귀함수를 활용한 구현
+ * 
  */
 
 package algorithm.graph;
 
-import java.util.LinkedList;
 import java.util.Stack;
 
-class Graph {
-    int size;
-    Node[] nodes;
+import algorithm.graph.Graph.Node;
 
-    public Graph(int size) {
-        this.size = size;
-        this.nodes = new Node[size];
+public class DFS {
+    Graph graph;
 
-        for (int i=0; i<size; i++) {
-            nodes[i] = new Node(i);
-        }
-    }
-
-    /**
-     * addEdge
-     *  - 두 노드간 간선 추가
-     */
-    public void addEdge(int a, int b) {
-        if (this.size < a || this.size < b) {
-            throw new Error();
-        }
-
-        Node n1 = nodes[a];
-        Node n2 = nodes[b];
-
-        if (!n1.adjacent.contains(n2)) {
-            n1.adjacent.add(n2);
-        }
-        if (!n2.adjacent.contains(n1)) {
-            n2.adjacent.add(n1);
-        }
+    public DFS(Graph graph) {
+        this.graph = graph;
     }
 
     /* 1. stack을 활용한 구현 */
@@ -48,11 +24,11 @@ class Graph {
     }
 
     public void dfs(int idx) {
-        if ((idx < 0) || (idx >= this.size)) {
+        if ((idx < 0) || (idx >= graph.getSize())) {
             throw new Error();
         }
 
-        Node node = this.nodes[idx];
+        Node node = graph.getNode(idx);
         dfs(node);
     }
 
@@ -62,14 +38,14 @@ class Graph {
         // 1. stack 할당 & 시작 노드를 넣으면 준비 끝
         Stack<Node> stack = new Stack<>();
         stack.push(node);
-        node.marked = true;  // 넣은 node는 마킹
+        node.marked = true; // 넣은 node는 마킹
 
         while (!stack.isEmpty()) {
-            Node tmp = stack.pop();  // 2. stack에서 node를 하나 꺼내서
+            Node tmp = stack.pop(); // 2. stack에서 node를 하나 꺼내서
 
-            /** 
+            /**
              * 3. 현재 노드와 간선으로 연결된 노드들 중 
-             *   마킹이 안돼있는 놈들만 stack에 넣고, 마킹
+             *    마킹이 안돼있는 놈들만 stack에 넣고, 마킹
              */
             for (Node n : tmp.adjacent) {
                 if (!n.marked) {
@@ -77,7 +53,7 @@ class Graph {
                     n.marked = true;
                 }
             }
-            
+
             // 4. 꺼낸 node는 print
             System.out.print(tmp.data + " ");
         }
@@ -89,11 +65,11 @@ class Graph {
     }
 
     public void dfsRecursive(int idx) {
-        if ((idx < 0) || (this.size <= idx)) {
+        if ((idx < 0) || (graph.getSize() <= idx)) {
             throw new Error();
         }
 
-        Node node = nodes[idx];
+        Node node = graph.getNode(idx);
         dfsRecursive(node);
     }
 
@@ -108,26 +84,12 @@ class Graph {
 
         // 3. 현재 노드와 간선으로 연결된 노드들 보기
         for (Node n : node.adjacent) {
-            if (!n.marked) {  // 4. 방문이 안된 곳이라면
+            if (!n.marked) { // 4. 방문이 안된 곳이라면
                 dfsRecursive(n);
             }
         }
     }
 
-    class Node {
-        int data;
-        boolean marked;
-        LinkedList<Node> adjacent;
-
-        Node(int data) {
-            this.data = data;
-            this.marked = false;
-            this.adjacent = new LinkedList<>();
-        }
-    }
-}
-
-public class DFS {
     public static void main(String[] args) {
         /**
          *     0
@@ -149,7 +111,8 @@ public class DFS {
         graph.addEdge(5, 7);
         graph.addEdge(6, 8);
 
-        graph.dfs();
-        //graph.dfsRecursive();
+        DFS dfs = new DFS(graph);
+        //dfs.dfs(7);
+        dfs.dfsRecursive(4);
     }
 }
