9d307101c6189c6078aa5d4c286f735138ac3d3b
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-21 18:19:54 +0900

    [Add] DirectedGraph.java

---
+++ algorithm/graph/DirectedGraph.java
@@ -0,0 +1,60 @@
+/**
+ * DirectedGraph
+ */
+
+package algorithm.graph;
+
+import java.util.LinkedList;
+
+public class DirectedGraph {
+    private int size;
+    private Node[] nodes;
+
+    public DirectedGraph(int size) {
+        this.size = size;
+        this.nodes = new Node[size];
+
+        for (int i=0; i<size; i++) {
+            nodes[i] = new Node(i);
+        }
+    }
+
+    /**
+     * addEdge
+     *  - 두 노드간 간선 추가
+     */
+    public void addEdge(int from, int to) {
+        if ((from < 0) || (to < 0) || 
+            (this.size < from) || (this.size < to)) {
+
+            throw new Error();
+        }
+
+        Node n1 = nodes[from];
+        Node n2 = nodes[to];
+
+        if (!n1.adjacent.contains(n2)) {
+            n1.adjacent.add(n2);
+        }
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public Node getNode(int idx) {
+        return this.nodes[idx];
+    }
+
+    class Node {
+        int data;
+        boolean marked;
+        LinkedList<Node> adjacent;
+
+        Node(int data) {
+            this.data = data;
+            this.marked = false;
+            this.adjacent = new LinkedList<>();
+        }
+    }
+}
