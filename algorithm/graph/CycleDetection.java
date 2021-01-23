229701e5e63d3a8d754c4aad75f2990b8760f1fe
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-23 22:29:10 +0900

    [Update] 어느정도 완성, 방향 그래프에선 작동 x, 미완
    	수정함:        algorithm/graph/CycleDetection.java

--- algorithm/graph/CycleDetection.java
+++ algorithm/graph/CycleDetection.java
@@ -8,31 +8,71 @@ import algorithm.graph.AbstGraph.Node;
 
 public class CycleDetection {
     private AbstGraph graph;
+    private boolean flag = false;
 
     boolean hasCycle(AbstGraph graph) {
-        if (graph instanceof DirectedGraph) {
-            this.graph = graph;
-            return hasCycleInDirectedGraph((DirectedGraph)this.graph);
-        } else if (graph instanceof UnDirectedGraph) {
-            this.graph = graph;
-            return hasCycleInUnDirectedGraph((UnDirectedGraph)this.graph);
+        if (!(graph instanceof DirectedGraph) && !(graph instanceof UnDirectedGraph)) {
+            throw new Error("CycleDetection::hasCycle(AbstGraph)");
         }
 
-        throw new Error("CycleDetection::hasCycle(AbstGraph)");
+        this.graph = graph;
+        detectCycle();
+
+        return this.flag;
     }
 
-    private boolean hasCycleInDirectedGraph(DirectedGraph directedGraph) {
-        assert (directedGraph instanceof DirectedGraph);
-
-        return false;
+    public void detectCycle() {
+        detectCycle(0);
     }
 
-    private boolean hasCycleInUnDirectedGraph(UnDirectedGraph unDirectedGraph) {
-        assert (unDirectedGraph instanceof UnDirectedGraph);
+    private void detectCycle(int idx) {
+        if ((idx < 0) || (graph.getSize() <= idx)) {
+            throw new Error("idx is must be between 0 and graph.getSize().");
+        }
 
-        /* back edge 검출 */
+        Node node = graph.getNode(idx);
 
-        return false;
+        if (graph instanceof DirectedGraph) {
+            detectCycleInDirectedGraphRecursive(node);
+        } else {
+            detectCycleInUnDirectedGraphRecursive(node, null);
+        }
+    }
+
+    private void detectCycleInDirectedGraphRecursive(Node curr) {
+        assert (curr != null);
+
+        // 1. 방문 마킹
+        curr.marked = true;
+
+        // 2. 현재 노드와 간선으로 연결된 노드들 보기
+        for (Node n : curr.adjacent) {
+            if (!n.marked) { // 3. 방문이 안된 곳이라면
+                detectCycleInDirectedGraphRecursive(n);
+            } else {
+                this.flag = true;
+                break;
+            }
+        }
+    }
+
+    private void detectCycleInUnDirectedGraphRecursive(Node curr, Node parent) {
+        assert (curr != null);
+
+        // 1. 방문 마킹
+        curr.marked = true;
+
+        // 2. 현재 노드와 간선으로 연결된 노드들 보기
+        for (Node n : curr.adjacent) {
+            if (!n.marked) { // 3. 방문이 안된 곳이라면
+                detectCycleInUnDirectedGraphRecursive(n, curr);
+            } else {
+                if (n != parent) {  // detect back edge
+                    this.flag = true;
+                    break;
+                }
+            }
+        }
     }
 
     public void dfsRecursive() {
@@ -41,7 +81,7 @@ public class CycleDetection {
 
     public void dfsRecursive(int idx) {
         if ((idx < 0) || (graph.getSize() <= idx)) {
-            throw new Error();
+            throw new Error("idx is must be between 0 and graph.getSize().");
         }
 
         Node node = graph.getNode(idx);
@@ -65,6 +105,42 @@ public class CycleDetection {
         }
     }
 
+    //private void dfsInDirectedGraphRecursive(Node curr) {
+    //    assert (curr != null);
+
+    //    // 1. 방문 마킹
+    //    curr.marked = true;
+
+    //    // 2. 현재 노드와 간선으로 연결된 노드들 보기
+    //    for (Node n : curr.adjacent) {
+    //        if (!n.marked) { // 3. 방문이 안된 곳이라면
+    //            dfsInDirectedGraphRecursive(n);
+    //        } else {
+    //            this.flag = true;
+    //            break;
+    //        }
+    //    }
+    //}
+
+    //private void dfsInUnDirectedGraphRecursive(Node curr, Node parent) {
+    //    assert (curr != null);
+
+    //    // 1. 방문 마킹
+    //    curr.marked = true;
+
+    //    // 2. 현재 노드와 간선으로 연결된 노드들 보기
+    //    for (Node n : curr.adjacent) {
+    //        if (!n.marked) { // 3. 방문이 안된 곳이라면
+    //            dfsInUnDirectedGraphRecursive(n, curr);
+    //        } else {
+    //            if (n != parent) {  // detect back edge
+    //                this.flag = true;
+    //                break;
+    //            }
+    //        }
+    //    }
+    //}
+
     public static void main(String[] args) {
         /**
          * UnDirectedGraph
@@ -75,15 +151,19 @@ public class CycleDetection {
          *     /  \
          *    2 -- 3 -- 4
          */
-        AbstGraph unDirectedGraph = new UnDirectedGraph(5);
-        unDirectedGraph.addEdge(0, 1);
-        unDirectedGraph.addEdge(1, 2);
-        unDirectedGraph.addEdge(1, 3);
-        unDirectedGraph.addEdge(2, 3);
-        unDirectedGraph.addEdge(3, 4);
+        //AbstGraph unDirectedGraph = new UnDirectedGraph(5);
+        AbstGraph directedGraph = new DirectedGraph(5);
+        directedGraph.addEdge(0, 1);
+        directedGraph.addEdge(1, 2);
+        directedGraph.addEdge(1, 3);
+        directedGraph.addEdge(2, 3);
+        directedGraph.addEdge(3, 4);
 
         CycleDetection cycleDetection = new CycleDetection();
-        cycleDetection.hasCycle(unDirectedGraph);
-        //cycleDetection.hasCycle(null);
+        if (cycleDetection.hasCycle(directedGraph)) {
+            System.out.println("싸이클 존재");
+        } else {
+            System.out.println("안존재");
+        }
     }
 }
