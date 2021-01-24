7f30e4ac58a3026d387582a21d7b6c903f038604
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-24 18:33:51 +0900

    [Update] impl all methods
    	수정함:        algorithm/graph/CycleDetection.java

--- algorithm/graph/CycleDetection.java
+++ algorithm/graph/CycleDetection.java
@@ -4,11 +4,14 @@
 
 package algorithm.graph;
 
+import java.util.Stack;
+
 import algorithm.graph.AbstGraph.Node;
 
 public class CycleDetection {
     private AbstGraph graph;
     private boolean flag = false;
+    Stack<Node> processStack = new Stack<>();
 
     public CycleDetection(AbstGraph graph) {
         this.graph = graph;
@@ -38,29 +41,34 @@ public class CycleDetection {
         Node node = graph.getNode(idx);
 
         if (graph instanceof DirectedGraph) {
-            //detectCycleInDirectedGraphRecursive(node);
+            detectCycleInDirectedGraphRecursive(node);
         } else {
             detectCycleInUnDirectedGraphRecursive(node, null);
         }
     }
 
-    // 버그, 엎어야 함.
-    //private void detectCycleInDirectedGraphRecursive(Node curr) {
-    //    assert (curr != null);
+    private void detectCycleInDirectedGraphRecursive(Node curr) {
+        assert (curr != null);
 
-    //    // 1. 방문 마킹
-    //    curr.marked = true;
+        // 1. 방문 마킹
+        curr.marked = true;
 
-    //    // 2. 현재 노드와 간선으로 연결된 노드들 보기
-    //    for (Node n : curr.adjacent) {
-    //        if (!n.marked) { // 3. 방문이 안된 곳이라면
-    //            detectCycleInDirectedGraphRecursive(n);
-    //        } else {
-    //            this.flag = true;
-    //            break;
-    //        }
-    //    }
-    //}
+        // 2. 현재 노드를 stack에 push
+        processStack.push(curr);
+
+        // 2. 현재 노드와 간선으로 연결된 노드들 보기
+        for (Node n : curr.adjacent) {
+            if (!n.marked) { // 3. 방문이 안된 곳이라면
+                detectCycleInDirectedGraphRecursive(n);
+            } else {
+                if (processStack.contains(n)) {  // 방문이 됐는데 진행중인 process라면 back edge
+                    this.flag = true;
+                    break;
+                }
+            }
+        }
+        processStack.pop();
+    }
 
     private void detectCycleInUnDirectedGraphRecursive(Node curr, Node parent) {
         assert (curr != null);
@@ -123,16 +131,42 @@ public class CycleDetection {
          *    2 -- 3 -- 4
          */
         //AbstGraph unDirectedGraph = new UnDirectedGraph(5);
-        AbstGraph directedGraph = new DirectedGraph(5);
+        //unDirectedGraph.addEdge(0, 1);
+        //unDirectedGraph.addEdge(1, 2);
+        //unDirectedGraph.addEdge(1, 3);
+        //unDirectedGraph.addEdge(2, 3);
+        //unDirectedGraph.addEdge(3, 4);
+
+        //CycleDetection cycleDetection = new CycleDetection(unDirectedGraph);
+
+        //if (cycleDetection.hasCycle(unDirectedGraph)) {
+        //    System.out.println("싸이클 존재");
+        //} else {
+        //    System.out.println("안존재");
+        //}
+
+        /**
+         * DirectedGraph
+         * 
+         *      0
+         *    ↙ ↘
+         *   1     2 → 3
+         *   ↓   ↓ ↖
+         *   4 →  5 → 6
+         * 
+         *  (2 -> 5 -> 6 cycle)
+         */
+        AbstGraph directedGraph = new DirectedGraph(7);
         directedGraph.addEdge(0, 1);
-        directedGraph.addEdge(1, 2);
-        directedGraph.addEdge(1, 3);
+        directedGraph.addEdge(0, 2);
+        directedGraph.addEdge(1, 4);
         directedGraph.addEdge(2, 3);
-        directedGraph.addEdge(3, 4);
+        directedGraph.addEdge(2, 5);
+        directedGraph.addEdge(4, 5);
+        directedGraph.addEdge(5, 6);
+        directedGraph.addEdge(6, 2);
 
         CycleDetection cycleDetection = new CycleDetection(directedGraph);
-
-        cycleDetection.dfsRecursive();
 
         if (cycleDetection.hasCycle(directedGraph)) {
             System.out.println("싸이클 존재");
