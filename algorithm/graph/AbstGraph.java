6cb0e84d6fcdb55eb10e2690a643cc63165bce89
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-27 02:25:36 +0900

    [Modify] remove size field & getter to abstract
    	수정함:        algorithm/graph/AbstGraph.java

--- algorithm/graph/AbstGraph.java
+++ algorithm/graph/AbstGraph.java
@@ -4,14 +4,9 @@ import java.util.List;
 import java.util.LinkedList;
 
 public abstract class AbstGraph {
-    private int size = 0;
-
-    public int getSize() {
-        return this.size;
-    }
-
     public abstract void addEdge(int a, int b);
     public abstract Node getNode(int idx);
+    public abstract int getSize();
 
     class Node {
         int data;
