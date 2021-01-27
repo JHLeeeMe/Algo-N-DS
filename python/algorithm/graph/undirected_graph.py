87226453efb37625b01b9fb80e2bb13d2fbd8a7f
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-27 16:28:18 +0900

    [Update] all methods
    	수정함:        python/algorithm/graph/directed_graph.py
    	수정함:        python/algorithm/graph/undirected_graph.py

--- python/algorithm/graph/undirected_graph.py
+++ python/algorithm/graph/undirected_graph.py
@@ -2,4 +2,27 @@ from .graph_base import GraphBase
 
 
 class UnDirectedGraph(GraphBase):
-    pass
+    def __init__(self, size: int):
+        self.__size = size
+        self.__nodes = list()
+
+    def add_edge(self, _from: int, _to: int):
+        if (_from < 0) or (_to < 0) or \
+                (self.__size < _from) or (self.__size < _to):
+            raise IndexError("")
+
+        n1 = self.nodes[_from]
+        n2 = self.nodes[_to]
+
+        if n2 not in n1.adjacent:
+            n1.adjacent.append(n2)
+        if n1 not in n2.adjacent:
+            n2.adjacent.append(n1)
+
+    @property
+    def node(self, idx: int):
+        return self.nodes[idx]
+
+    @property
+    def size(self) -> int:
+        return self.__size
