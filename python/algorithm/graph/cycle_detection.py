62923e5faa4af94c600a4f414b40f8214bb1b1ba
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-28 18:10:55 +0900

    [Update] method (undirected graph의 경우) 완성
    	수정함:        python/algorithm/graph/cycle_detection.py

--- python/algorithm/graph/cycle_detection.py
+++ python/algorithm/graph/cycle_detection.py
@@ -37,5 +37,17 @@ class CycleDetection:
     def __detect_cycle_in_directed_graph_recursive(self, curr: Node):
         pass
 
-    def __detect_cycle_in_undirected_graph_recursive(self, curr: Node, prev: Node):
-        pass
+    def __detect_cycle_in_undirected_graph_recursive(self, curr: Node, prev: Node = None):
+        assert (curr is not None)
+
+        # 1. 현재 노드 마킹
+        curr.marked = True
+
+        # 2. 현재 노드와 간선으로 연결된 노드들 보기
+        for n in curr.adjacent:
+            if n.marked is False:  # 3. 마킹이 안된 노드라면
+                self.__detect_cycle_in_undirected_graph_recursive(curr=n, prev=curr)
+            else:
+                if n != prev:  # detect back edge
+                    self.flag = True
+                    break
