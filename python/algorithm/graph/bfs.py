535c1d31822f358712605903bd89b645fc879c5d
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-29 19:00:30 +0900

    [Add] bfs.py
    	수정함:        python/algorithm/graph/bfs.py

--- python/algorithm/graph/bfs.py
+++ python/algorithm/graph/bfs.py
@@ -11,4 +11,31 @@ class BFS:
             raise TypeError()
 
         self.graph = graph
-        self.queue = Queue()
+
+    def bfs(self, idx: int = 0):
+        if (idx < 0) or (idx >= self.graph.size):
+            raise IndexError("0 <= idx < self.graph.size")
+
+        node = self.graph.node_at(idx)
+        self.__bfs(node)
+
+    def __bfs(self, node: Node):
+        assert (node is not None)
+
+        # 1. queue 생성 & 시작 노드 put
+        queue = Queue()
+        queue.put(node)
+        node.marked = True  # 추가된 노드 마킹
+
+        while not queue.empty():
+            tmp_node = queue.get()  # 2. queue 에서 노드를 하나 꺼내서
+
+            # 3. 꺼낸 노드와 간선으로 견결된 노드들 중
+            #    마킹이 안돼있는 놈들만 queue 넣고, 마킹
+            for n in tmp_node.adjacent:
+                if n.marked is False:
+                    queue.put(n)
+                    n.marked = True
+
+            # 4. 꺼낸 노드는 print
+            print(str(tmp_node.data), end=" ")
