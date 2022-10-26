e250d8fad7b6ef011e36b8d4510a54223be31939
JHLeeeMe <lejung92@gmail.com>
2022-10-27 07:24:14 +0900

    [Add] ```queue.cpp```
    	수정함:        cpp/data_structure/queue.cpp

--- cpp/data_structure/queue.cpp
+++ cpp/data_structure/queue.cpp
@@ -1,48 +1,171 @@
+#define Assert(expression) assert(expression)
+#define Assert_msg(expression, message) assert(expression && message)
+
 #include <iostream>
 #include <cassert>
 #include <array>
 
-#define Assert(expression) assert(expression)
-#define Assert_msg(expression, message) assert(expression && message)
+#include "node.h"
 
-template<typename T, int arr_size>
+template<typename T>
 class Queue
 {
 public:
     Queue();
-    Queue(const Queue<T, arr_size>& q);
-    //~Queue();
+    Queue(const Queue<T>& q)=delete;
+    ~Queue();
 
-    unsigned int get_max_size() const;
+    void enqueue(T data);
+    T dequeue();
+    const T& peek() const;
+
+    const unsigned int get_size() const;
+    const bool is_empty() const;
+    void clear();
 private:
-    T* _arr_ptr;
-    unsigned int _max_size;
+    jhleeeme::Node<T>* _create_node(T data) const;
+private:
+    jhleeeme::Node<T>* _front;
+    jhleeeme::Node<T>* _rear;
+    unsigned int _size;
 };
 
-template<typename T, int arr_size>
-Queue<T, arr_size>::Queue()
-    : _arr_ptr(new T[arr_size])
-    , _max_size(arr_size)
+template<typename T>
+Queue<T>::Queue()
+    : _front(nullptr)
+    , _rear(nullptr)
+    , _size(0)
 {}
 
-template<typename T, int arr_size>
-Queue<T, arr_size>::Queue(const Queue<T, arr_size>& q)
+template<typename T>
+Queue<T>::~Queue()
 {
-    T* new_arr = new T[_max_size];
-    for (int i = 0; i < _max_size; i++)
+    clear();
+}
+
+template<typename T>
+void Queue<T>::enqueue(T data)
+{
+    jhleeeme::Node<T>* new_node = _create_node(data);
+    if (_size == 0)
     {
-        new_arr[i] = *_arr_ptr[i];
+        _front = new_node;
+    }
+    else
+    {
+        new_node->prev = _rear;
+        _rear->next = new_node;
+    }
+    _rear = new_node;
+    _size++;
+}
+
+template<typename T>
+T Queue<T>::dequeue()
+{
+    Assert_msg(_size != 0, "Queue is Empty!");
+
+    jhleeeme::Node<T>* tmp = _front;
+    T ret = tmp->data;
+    _front = _front->next;
+    delete tmp;
+    _size--;
+
+    return ret;
+}
+
+template<typename T>
+const T& Queue<T>::peek() const
+{
+    Assert_msg(_size != 0, "Queue is Empty!");
+
+    return _front->data;
+}
+
+template<typename T>
+const unsigned int Queue<T>::get_size() const
+{
+    return _size;
+}
+
+template<typename T>
+const bool Queue<T>::is_empty() const
+{
+    return _size == 0;
+}
+
+template<typename T>
+void Queue<T>::clear()
+{
+    if (_size == 0)
+    {
+        return;
+    }
+    else
+    {
+        while (_size != 0)
+        {
+            jhleeeme::Node<T>* tmp = _front;
+            _front = _front->next;
+            delete tmp;
+            _size--;
+        }
     }
 }
-template<typename T, int arr_size>
-unsigned int Queue<T, arr_size>::get_max_size() const
+
+template<typename T>
+jhleeeme::Node<T>* Queue<T>::_create_node(T data) const
 {
-    return _max_size;
+    jhleeeme::Node<T>* new_node = new jhleeeme::Node<T>;
+    new_node->prev = nullptr;
+    new_node->next = nullptr;
+    new_node->data = data;
+    return new_node;
 }
 
 int main()
 {
-    Queue<int, 30> q;
-    Assert(q.get_max_size() == 30);
+    {
+        Queue<int> q;
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        Assert(q.get_size() == 3);
+        Assert(q.dequeue() == 10);
+        Assert(q.get_size() == 2);
+        Assert(q.dequeue() == 20);
+        Assert(q.get_size() == 1);
+        Assert(q.dequeue() == 30);
+        q.enqueue(50);
+        q.enqueue(100);
+        q.enqueue(92);
+        Assert(q.get_size() == 3);
+        Assert(q.dequeue() == 50 && q.get_size() == 2);
+    }
+    {
+        Queue<int> q;
+        for (int i = 0; i < 10; i++)
+        {
+            q.enqueue(i);
+        }
+        Assert(q.get_size() == 10);
+        Assert(q.dequeue() == 0);
+        q.clear();
+        q.clear();
+        Assert(q.get_size() == 0);
+        Assert(q.is_empty());
+    }
+    {
+        Queue<int> q;
+        for (int i = 0; i < 10; i++)
+        {
+            q.enqueue(i);
+        }
+        Assert(!q.is_empty());
+        Assert(q.peek() == 0);
+        Assert(q.peek() == 0);
+        Assert(q.get_size() == 10);
+    }
+
     return 0;
 }
