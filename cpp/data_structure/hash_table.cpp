4ce71f2cac743e9e281c4ab258f268fc7485ea9f
JHLeeeMe <lejung92@gmail.com>
2022-11-01 09:03:10 +0900

    [Update] ```put(...)``` 추가 & ```__ASSERT``` 전처리문 추가
    	수정함:        cpp/data_structure/hash_table.cpp

--- cpp/data_structure/hash_table.cpp
+++ cpp/data_structure/hash_table.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 #include <typeinfo>
 #include <vector>
 #include <cstring>
+#include <cassert>
 
-#define __INIT_CAPACITY 16
+#define __ASSERT(expr) assert(expr)
+#define __ASSERT_MSG(expr, msg) assert(expr && msg)
+
+#define __DEFAULT_CAPACITY 16
 
 template<typename T>
 struct Node
@@ -17,24 +22,22 @@ class HashTable
 {
 public:
     HashTable()
-        : _table(std::vector<Node<T>*>(__INIT_CAPACITY))
-        , _capacity(__INIT_CAPACITY){}
+        : _table(std::vector<Node<T>*>(__DEFAULT_CAPACITY))
+        , _capacity(__DEFAULT_CAPACITY) {}
     HashTable(size_t capacity)
         : _table(std::vector<Node<T>*>(capacity))
         , _capacity(capacity) {}
     //HashTable(const HashTable& h);
-    //HashTable(HashTable&& htb);
+    //HashTable(HashTable&& h);
     //~HashTable();
 public:
     size_t get_capacity() const;
-    //float get_load_factor() const;
-    size_t hash(T data) const;
     void put(T data);
-    //bool find(T data) const;
+private:
+    size_t _hash(const std::string& data) const;
 private:
     std::vector<Node<T>*> _table;
     size_t _capacity;
-    float _load_factor;
 };
 
 template<typename T>
@@ -44,86 +47,69 @@ size_t HashTable<T>::get_capacity() const
 }
 
 template<typename T>
-size_t HashTable<T>::hash(T data) const
+size_t HashTable<T>::_hash(const std::string& data) const
 {
-    size_t str_len = 0;
-    // hasing
-    if (strcmp(typeid(data).name(), "PKc") == 0)
+    // djb2 function
+    // (http://www.cse.yorku.ca/~oz/_hash.html)
+    unsigned long _hash = 5381;
+    int c;
+    for (int i = 0; i < data.length(); i++)
     {
-        str_len = char_arr_length(data);
+        c = data[i];
+        _hash = ((_hash << 5) + _hash) + c;  // _hash * 33 + c
     }
-    else
-    {
-        str_len = data.length();
-    }
-    size_t buf[str_len];
-    int* ascii_buf = stoi(buf, data);
-    // 미완
 
-    return 0;
+    return _hash % _capacity;
 }
 
 template<typename T>
 void HashTable<T>::put(T data)
 {
-    // idx = hash(data)
-    // vec[idx]
-    // push_back new node
-}
+    Node<T>* new_node = new Node<T>;
+    new_node->next = nullptr;
+    new_node->value = data;
 
-void stoi(int* buf, const std::string& s)
-{
-    for (int i = 0; i < s.length(); i++)
+    size_t idx = _hash(data);
+    if (_table[idx] == nullptr)
     {
-        buf[i] = int(s[i]);
+        _table[idx] = new_node;
     }
-}
-
-size_t char_arr_length(const char* s)
-{
-    size_t cnt = 0;
-    while (*s != '\0')
+    else
     {
-        s++;
-        cnt++;
+        Node<T>* prev = nullptr;
+        Node<T>* curr = _table[idx];
+        while (curr != nullptr)
+        {
+            if (curr->value == data)
+            {
+                std::cout << "key: \"" << curr->value << "\" 중복" << std::endl;
+                delete new_node;
+                return;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = new_node;
     }
-    return cnt;
 }
 
 int main()
 {
-    HashTable<int> h;
-    std::cout << h.get_capacity() << std::endl;
-
-    {  // string to int Test
-        const char* s1 = "abcd";
-        size_t s1_len = char_arr_length(s1);
-        int ascii_buf1[s1_len];
-        //stoi(ascii_buf1, s1, s1_len);
-        stoi(ascii_buf1, s1);
-
-        const std::string s2 = "abcd";
-        int ascii_buf2[s2.length()];
-        stoi(ascii_buf2, s2);
-
-        std::cout << "-------- ascii_buf1 -------------" << std::endl;
-        for (const auto& e : ascii_buf1)
-        {
-            std::cout << e << std::endl;
-        }
-        std::cout << "-------- ascii_buf2 -------------" << std::endl;
-        for (const auto& e : ascii_buf2)
-        {
-            std::cout << e << std::endl;
-        }
+    HashTable<int> h_int;
+    HashTable<std::string> h_string(10);
+    {  // capacity Test
+        __ASSERT(h_int.get_capacity() == __DEFAULT_CAPACITY);
+        __ASSERT(h_string.get_capacity() == 10);
     }
+    {
+        h_string.put("a");
+        h_string.put("a");
+        h_string.put("a");
 
-    int a;
-    double b;
-    std::string s = "sdfsdf";
-    std::cout << typeid(a).name() << std::endl;
-    std::cout << typeid(s).name() << std::endl;
-    std::cout << typeid(b).name() << std::endl;
+        h_string.put("bca");
+        h_string.put("cba");
+        h_string.put("kkka");
+    }
 
     return 0;
 }
