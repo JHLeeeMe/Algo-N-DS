4daee36b89d35c0580060d11741eea2951ab25d1
JHLeeeMe <lejung92@gmail.com>
2022-11-16 00:07:35 +0900

    [Add] touch quick_sort.cpp
    	새 파일:       cpp/algorithm/sort/quick_sort.cpp

---
+++ cpp/algorithm/sort/quick_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+void quick_sort(int arr[], int size);
+int partition(int arr[], int left, int right);
+void sort(int arr[], int left, int right);
+void swap(int arr[], int a, int b);
+
+int main()
+{
+    return 0;
+}
+
+void quick_sort(int arr[], int size)
+{}
+
+int partition(int arr[], int left, int right)
+{
+    return 0;
+}
+
+void sort(int arr[], int left, int right)
+{}
+
+void swap(int arr[], int a, int b)
+{}
